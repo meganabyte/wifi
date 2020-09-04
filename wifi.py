cfc1d37cfc9678ab44eea15570fbedb8001b6a5f
Megana Bobba <51874806+meganabyte@users.noreply.github.com>
2020-09-03 18:49:08 -0700

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,37 +1,8 @@
 import random
 import math
+from event import Event
+from priority_queue import PriorityQueue
 
-class Event:
-  def __init__(self, time, eventCategory):
-    self.time = time
-    self.event_category = eventCategory
-  
-# Priority Queue Class
-class PriorityQueue(object): 
-    def __init__(self): 
-        self.queue = [] 
-  
-    def __str__(self): 
-        return ' '.join([str(i) for i in self.queue]) 
-  
-    def isEmpty(self): 
-        return len(self.queue) == 0 
-  
-    def insert(self, data): 
-        self.queue.append(data) 
-  
-    def delete(self): 
-        try: 
-            min = 0
-            for i in range(len(self.queue)): 
-                if self.queue[i].time < self.queue[min].time: 
-                    min = i 
-            item = self.queue[min] 
-            del self.queue[min] 
-            return item 
-        except IndexError: 
-            print("Index Error") 
-            exit()
 
 # global variables
 
